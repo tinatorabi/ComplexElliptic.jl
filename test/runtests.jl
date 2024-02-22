c6991e971b9b252e729c4c5df7d2e7454be4030f
Tina Torabi <tntorabii@gmail.com>
2024-02-22 00:51:06 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,12 +16,12 @@ end
 @testset "ellipjc Function" begin
     u = 0.5
     L = 1.0
-    sn, cn, dn = ComplexElliptic.ellipjc(u, L)  # Pass a boolean flag
+    sn, cn, dn = ComplexElliptic.ellipjc(u, L)
     # Verify the types of the returned values
     @test typeof(sn) == Complex{Float64}
     @test typeof(cn) == Complex{Float64}
     @test typeof(dn) == Complex{Float64}
-    @test round(abs(sn), digits=4) ≈ 0.4794
-    @test round(abs(cn), digits=4) ≈ 0.8776
-    @test round(abs(dn), digits=4) ≈ 0.9998
+    @test round(abs(sn[1]), digits=4) ≈ 0.4794
+    @test round(abs(cn[1]), digits=4) ≈ 0.8776
+    @test round(abs(dn[1]), digits=4) ≈ 0.9998
 end
